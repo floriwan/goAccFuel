2be5d96fe93b68d4591d453cfe0ad65fd66ebd17
floriwan <nospam@goeldenitz.org>
2022-03-02 11:38:47 +0100

    header insets

--- widgets/header.go
+++ widgets/header.go
@@ -35,55 +35,67 @@ func (h HeaderInfoStyle) Layout(gtx C) D {
 
 			paint.ColorOp{Color: h.textColor}.Add(gtx.Ops)
 			return layout.Flex{}.Layout(gtx,
-				layout.Rigid(func(gtx C) D {
-					return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "Session")
-				}),
-				layout.Rigid(func(gtx C) D {
-
-					return layout.Flex{
-						Axis:      layout.Vertical,
-						Alignment: layout.End,
-					}.Layout(gtx,
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Session")
-						}),
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "95 min")
-						}),
-					)
-
-				}),
-
+				//layout.Rigid(func(gtx C) D {
+				//	return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "Session")
+				//}),
 				layout.Rigid(func(gtx C) D {
 
-					return layout.Flex{
-						Axis:      layout.Vertical,
-						Alignment: layout.End,
-					}.Layout(gtx,
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Laps")
-						}),
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "73")
-						}),
-					)
+					return layout.Inset{
+						Left:  unit.Dp(5),
+						Right: unit.Dp(5),
+					}.Layout(gtx, func(gtx C) D {
+						return layout.Flex{
+							Axis:      layout.Vertical,
+							Alignment: layout.End,
+						}.Layout(gtx,
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Session")
+							}),
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "95min")
+							}),
+						)
+					})
 
 				}),
 
 				layout.Rigid(func(gtx C) D {
 
-					return layout.Flex{
-						Axis:      layout.Vertical,
-						Alignment: layout.End,
-					}.Layout(gtx,
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Fuel")
-						}),
-						layout.Rigid(func(gtx C) D {
-							return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "145 l")
-						}),
-					)
+					return layout.Inset{
+						Left:  unit.Dp(5),
+						Right: unit.Dp(5),
+					}.Layout(gtx, func(gtx C) D {
+						return layout.Flex{
+							Axis:      layout.Vertical,
+							Alignment: layout.End,
+						}.Layout(gtx,
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Laps")
+							}),
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "73")
+							}),
+						)
+					})
+				}),
 
+				layout.Rigid(func(gtx C) D {
+
+					return layout.Inset{
+						Left: unit.Dp(5),
+					}.Layout(gtx, func(gtx C) D {
+						return layout.Flex{
+							Axis:      layout.Vertical,
+							Alignment: layout.End,
+						}.Layout(gtx,
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize.Scale(0.5), "Fuel")
+							}),
+							layout.Rigid(func(gtx C) D {
+								return widget.Label{}.Layout(gtx, h.textShaper, h.textFont, h.textFontSize, "145l")
+							}),
+						)
+					})
 				}),
 			)
 
